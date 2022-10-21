a0cdc6d8f9e344a0e8035f567ab800aba9718ee8
moris96 <moriskhoudari@gmail.com>
2022-10-21 12:42:34 -0400

    created repo and updated readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Algorithm practice for Leetcode
+
+![](https://external-preview.redd.it/kyQIEgY3wXkJYauoQQerIJPJuwRM0o1ksl3bcnOXZi8.png?auto=webp&s=4c2a58ad08003dd19bcea7abf8df75e6ee88a4f6)
---
+++ algos.js
@@ -0,0 +1,72 @@
+//Roman to integer 
+const romanToInt = (s) => {
+    let nums = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    let res = 0
+    for(let i=0; i<s.length; i++){
+        if(nums[s[i]] < nums[s[i+1]]){
+            res -= nums[s[i]]
+        } else{
+            res += nums[s[i]]
+        }
+    }
+    return res
+};
+
+x = "III" 
+// console.log(romanToInt(x))
+
+
+//Palindrome number 
+/*
+const isPalindrome = (x) => {
+    if(x < 0) return false 
+    let rev = 0
+    let y = x
+    while(y>0){
+        const lastInt = y % 10
+        rev = (rev * 10) + lastInt
+        y = (y / 10) | 0
+    }
+    return x === rev
+}
+n = 121
+// console.log(isPalindrome(n))
+*/
+
+
+// Valid Palindrome (string)
+const isPalindrome = (s) => {
+    return s == s.split('').reverse().join('')
+};
+// console.log(isPalindrome("race a car"))
+// console.log(isPalindrome("lol"))
+// console.log(isPalindrome("amanaplanacanalpanama"))
+
+
+//FizzBuzz (the most classic lol)
+const fizzBuzz = (n) => {
+    let output = []
+    for(let i = 1; i <= n; i++){
+        if(i % 15 == 0){
+            output.push("FizzBuzz")
+        } else if(i % 3 == 0){
+            output.push("Fizz")
+        } else if(i % 5 == 0){
+            output.push("Buzz") 
+        } else{
+            output.push(i.toString())
+        }
+    }
+    return output
+};
+console.log(fizzBuzz(15))
+
+
