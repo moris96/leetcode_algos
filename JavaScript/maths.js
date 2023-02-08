468a735b808f21a53d41237e5529fa871334a9a6
moris96 <moriskhoudari@gmail.com>
2023-02-08 12:43:09 -0500

    pow and reverse string algos

---
+++ JavaScript/maths.js
@@ -0,0 +1,40 @@
+//power of four
+// function isPowerOfFour(n){
+//     x = Math.log(n) / Math.log(4)
+//     return x === Math.floor(x)
+// }
+
+// function isPowerOfFour(n){
+//     if(n <= 0) return false 
+//     while(n > 1){
+//         if(n % 4 != 0) return false 
+//         n = n/4 
+//     }
+//     return true 
+// }
+// console.log(isPowerOfFour(0))
+
+
+//power of three
+// function isPowerOfThree(n){
+//     if(n <= 0) return false 
+//     while(n > 1){
+//         if(n % 3 != 0) return false 
+//         n = n/3
+//     }
+//     return true 
+// }
+// console.log(isPowerOfThree(-1)) //returns false 
+
+
+
+//power of two 
+// function isPowerOfTwo(n){
+//     if(n <= 0) return false 
+//     while(n > 1){
+//         if(n % 2 != 0) return false 
+//         n = n/2
+//     }
+//     return true 
+// }
+// console.log(isPowerOfTwo(3))
---
+++ JavaScript/strings.js
@@ -0,0 +1,5 @@
+//reverse a string 
+// function reverseString(s){
+//     return s.reverse()
+// }
+// console.log(reverseString(["h","e","l","l","o"]))
