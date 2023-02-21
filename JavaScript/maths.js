fcc496e45c16f3fc1aba181817e790968df89517
moris96 <moriskhoudari@gmail.com>
2023-02-21 13:33:29 -0500

    maths: single number

--- JavaScript/maths.js
+++ JavaScript/maths.js
@@ -140,3 +140,15 @@
 // }
 // console.log(toHex(-1)) //ffffffff
 // console.log(toHex(26)) //1a
+
+
+
+//single number
+// function singleNumber(nums){
+//     let res = 0
+//     for(let i in nums){
+//         res ^= nums[i]
+//     }
+//     return res 
+// }
+// console.log(singleNumber([2,2,1])) 
