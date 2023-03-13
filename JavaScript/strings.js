e65a6fa3486dbce415a14927d0a8577e9ce31562
moris96 <moriskhoudari@gmail.com>
2023-03-13 15:15:12 -0400

    longest palindrome: JS strings

--- JavaScript/strings.js
+++ JavaScript/strings.js
@@ -26,9 +26,27 @@
 
 
 //valid anagram
-function isAnagram(s, t){
-    if(s.length !== t.length) return false 
-    return s.split('').sort().join('') === t.split('').sort().join('') ? true : false;
-}
+// function isAnagram(s, t){
+//     if(s.length !== t.length) return false 
+//     return s.split('').sort().join('') === t.split('').sort().join('') ? true : false;
+// }
 
-console.log(isAnagram("anagram", "nagaram"))
+// console.log(isAnagram("anagram", "nagaram"))
+
+
+
+//longest palindrome 
+// const longestPalindrome = (s) => {
+//     const m = new Map();
+//     for (const element of s) {
+//         m.set(element, m.get(element) + 1 || 1);
+//     }
+//     let numOfOdds = 0;
+//     for (const value of m.values()) {
+//         if (value % 2 === 1) {
+//             numOfOdds++;
+//         }
+//     }
+//     return numOfOdds > 0 ? s.length - numOfOdds + 1 : s.length;
+// }
+// console.log(longestPalindrome("ccc")) 
