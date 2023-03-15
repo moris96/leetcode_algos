a7f040f29e685510c92e2571ff766d7d57abcf6f
moris96 <moriskhoudari@gmail.com>
2023-03-15 14:21:57 -0400

    Py: longest palindromic substring

--- Python/algos.py
+++ Python/algos.py
@@ -14,28 +14,28 @@ class Solution:
                 output.append(str(i))
         return output
 """
-def fizzBuzz(n):
-    output = []
-    for i in range(1, n + 1):
-        if i % 15 == 0:
-            output.append("FizzBuzz")
-        elif i % 3 == 0:
-            output.append("Fizz")
-        elif i % 5 == 0:
-            output.append("Buzz")
-        else:
-            output.append(str(i))
-    return output
+# def fizzBuzz(n):
+#     output = []
+#     for i in range(1, n + 1):
+#         if i % 15 == 0:
+#             output.append("FizzBuzz")
+#         elif i % 3 == 0:
+#             output.append("Fizz")
+#         elif i % 5 == 0:
+#             output.append("Buzz")
+#         else:
+#             output.append(str(i))
+#     return output
 # print(f'FizzBuzz solution for 3 is: {fizzBuzz(3)} ; for 5 is: {fizzBuzz(5)} ; for 15 is: {fizzBuzz(15)}')
 
 
 #math sqrt(x)
-def mySqrt(x):
-    return int((x ** 0.5) // 1)
+# def mySqrt(x):
+#     return int((x ** 0.5) // 1)
 #print(mySqrt(8))
 
 
 #math pow(x,n)
-def myPow(x, n):
-    return x ** n
+# def myPow(x, n):
+#     return x ** n
 # print(myPow(2,-2))
